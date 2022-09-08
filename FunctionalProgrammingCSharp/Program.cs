0699bddb35e35d573b712b5df0ef6a0d33dcec2c
mbayerworking <mbayer.work@gmail.com>
2022-09-08 13:50:13 -0700

    Initial commit

---
+++ FunctionalProgrammingCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using FunctionalProgrammingCSharp;
+
+Console.WriteLine();
+foreach (var s in args)
+{
+    Console.Write(s);
+    Console.Write(' ');
+}
+Console.WriteLine();
+
+await Utilities.ShowConsoleAnimation();
+
+Console.WriteLine();
+
+string[] answers =
+{
+    "It is certain.",       "Reply hazy, try again.",     "Don't count on it.",
+    "It is decidedly so.",  "Ask again later.",           "My reply is no.",
+    "Without a doubt.",     "Better not tell you now.",   "My sources say no.",
+    "Yes – definitely.",    "Cannot predict now.",        "Outlook not so good.",
+    "You may rely on it.",  "Concentrate and ask again.", "Very doubtful.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+};
+
+var index = new Random().Next(answers.Length - 1);
+Console.WriteLine(answers[index]);
+
+Func<int, int> triple = x => x * 3;
+var range = Enumerable.Range(1, 3);
+var triples = range.Select(triple);
+triples.ToList().ForEach(Console.WriteLine);
---
+++ FunctionalProgrammingCSharp/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingCSharp
+{
+    public static class Utilities
+    {
+        public static async Task ShowConsoleAnimation()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("| -");
+                await Task.Delay(50);
+                Console.Write("\b\b\b");
+                Console.Write("/ \\");
+                await Task.Delay(50);
+                Console.Write("\b\b\b");
+                Console.Write("- |");
+                await Task.Delay(50);
+                Console.Write("\b\b\b");
+                Console.Write("\\ /");
+                await Task.Delay(50);
+                Console.Write("\b\b\b");
+            }
+            Console.WriteLine();
+        }
+    }
+}
